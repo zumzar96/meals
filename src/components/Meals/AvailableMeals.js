af356fdbec70dccdbac1642bf11bd42bb546e1fa
ismail <ismailgim96@gmail.com>
2021-12-28 12:38:26 +0100

    Adding card for meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from '../UI/Card';
+
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -32,7 +34,9 @@ const AvailableMeals = () => {
 
     return (
         <section className={classes.meals}>
-            <ul>{mealsList}</ul>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
         </section>
     );
 };
