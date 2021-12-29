11e3df433a92c29cbb846fd6bbc57fd1614378cd
ismail <ismailgim96@gmail.com>
2021-12-29 14:21:14 +0100

    Finishing with cart modal

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     };
   return (
           <Fragment>
-          {cartIsShown && <Cart />}
+          {cartIsShown && <Cart onClose={hideCartHandler} />}
           <Header onShowCart={showCartHandler} />
           <main>
               <Meals />
