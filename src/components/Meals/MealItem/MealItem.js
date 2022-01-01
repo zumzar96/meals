fd228bbface6b240f77f2cd033a3eb99e4baff70
ismail <ismailgim96@gmail.com>
2022-01-01 14:03:48 +0100

    Outputing cart items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
-import classes from './MealItem.module.css';
-import MealItemForm from './MealItemForm';
+import { useContext } from 'react';
 
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -12,7 +25,8 @@ const MealItem = (props) => {
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
-            <div><MealItemForm />
+            <div>
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,5 @@
 import { useReducer } from 'react';
-
+ 
 
 import CartContext from './cart-context';
 
